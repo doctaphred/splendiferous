f03ec157b2b8a39954ee493e500f7f77eb6be8fa
Frederick Wagner <fwagner@quantopian.com>
2020-01-04 12:42:13 -0500

    Initial commit

---
+++ src/splendor/main.py
@@ -0,0 +1,56 @@
+from collections import namedtuple
+
+
+colors = tuple('wbgrk')
+
+
+class Gemset(namedtuple('Gemset', colors)):
+    __slots__ = ()
+
+    def __new__(cls, w=0, b=0, g=0, r=0, k=0):
+        return super().__new__(cls, w, b, g, r, k)
+
+    def __init__(self, *args, **kwargs):
+        assert all(isinstance(c, int) for c in self)
+
+    def __neg__(self):
+        return type(self)(*map(int.__neg__, self))
+
+    def __add__(self, other):
+        return type(self)(*map(int.__add__, self, other))
+
+    def __sub__(self, other):
+        return type(self)(*map(int.__sub__, self, other))
+
+    def __mul__(self, other):
+        return type(self)(*map(int.__mul__, self, other))
+
+    def __div__(self, other):
+        return type(self)(*map(int.__div__, self, other))
+
+    def __le__(self, other):
+        return all(map(int.__le__, self, other))
+
+    def __ge__(self, other):
+        return all(map(int.__ge__, self, other))
+
+    def __lt__(self, other):
+        return self <= other and self != other
+
+    def __gt__(self, other):
+        return self >= other and self != other
+
+    def __abs__(self):
+        return sum(self * self) ** 0.5
+
+
+class Card(namedtuple('Card', ['color', 'points', 'cost'])):
+    __slots__ = ()
+
+    def __new__(cls, color, points=0, cost=Gemset()):
+        return super().__new__(cls, color, points, cost)
+
+    def __init__(self, *args, **kwargs):
+        assert self.color in colors, self.color
+        assert isinstance(self.points, int), self.points
+        assert isinstance(self.cost, Gemset), self.cost
